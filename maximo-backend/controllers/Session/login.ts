c2485a088316e37d878f1cf1d5666364d0efee0c
Leonardo Oliveira Balsalobre <57500163+oliveirabalsa@users.noreply.github.com>
2020-05-23 20:28:25 -0300

    create: login bcrypt

--- maximo-backend/controllers/Session/login.ts
+++ maximo-backend/controllers/Session/login.ts
@@ -1,9 +1,9 @@
 import {
-    HandlerFunc,
-    Context,
+  HandlerFunc,
+  Context,
 } from "https://deno.land/x/abc@v1.0.0-rc2/mod.ts";
 import * as bcrypt from "https://deno.land/x/bcrypt/mod.ts";
-import Users from '../../model/users.ts'
+import Users from "../../model/users.ts";
 
 import connectionDatabase from "../../database/connection.ts";
 import { ErrorHandler } from "../../utils/handleError.ts";
@@ -12,52 +12,30 @@ const database = connectionDatabase.findDatabase;
 const user = database.collection("users");
 
 export const loginUser: HandlerFunc = async (data: Context) => {
-    try {
-        if (data.request.headers.get("content-type") !== "application/json") {
-            throw new ErrorHandler("Body invalido", 422);
-        }
-        const body = await (data.body());
-
-        console.log('body :>> ', body);
-        if (!Object.keys(body).length) {
-            throw new ErrorHandler("O body não pode estar vazio!!", 400);
-        }
-        let { email, password } = body;
-
-        const existUser: Users[] = await user.find();
-
-
-        if (existUser) {
-            const list = existUser.length
-                ? existUser.map((item: any) => {
-                    const { _id: { $oid }, name, lastName, email, password } = item;
-                    console.log('item :>> ', item);
-                    return { id: $oid, name, lastName, email, password };
-                }) : [];
-
-            list.find((userReturned) => {
-
-                let passwordSecurity = password as string;
-
-                const passwordHash = await bcrypt.compare(passwordSecurity, userReturned.password);
-
-                let passwordReturned = passwordHash as string;
-
-                if ((email === userReturned.email) && (passwordReturned === userReturned.password)) {
-
-                    await user.insertOne({ email, password });
-
-                    return data.json('Usuário Logado', 200);
-
-                }
-            })
-            // return data.json(list, 200);
-        }
-
-
-
-    } catch (error) {
-        throw new ErrorHandler(error.message, error.status || 500);
+  try {
+    if (data.request.headers.get("content-type") !== "application/json") {
+      throw new ErrorHandler("Body invalido", 422);
     }
-};
+    const body = await data.body();
 
+    console.log("body :>> ", body);
+    if (!Object.keys(body).length) {
+      throw new ErrorHandler("O body não pode estar vazio!!", 400);
+    }
+    let { email, password } = body;
+
+    const existUser: Users[] = await user.find();
+    existUser.find((item: any) => {
+      if (email === item.email) {
+        const compare = async () => {
+          return await bcrypt.compare(password as string, item.password);
+        };
+        if (compare()) {
+          return data.json({id: item._id.$oid}, 200);
+        }
+      }
+    });
+  } catch (error) {
+    throw new ErrorHandler(error.message, error.status || 500);
+  }
+};
